1ae12eb9b6604385b12cd3de141683aeb881b986
patriciaderieux <6089242+patty345@users.noreply.github.com>
2021-12-19 17:53:23 -0600

    added display of the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,4 +48,18 @@ function setNextQuestion () {
     showQuestion(QuestionsSuffled[currentQuestionsIndex]);
 };
 
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
+};
+
 
