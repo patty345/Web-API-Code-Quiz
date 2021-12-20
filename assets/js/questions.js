80e535f58005fac911ec875acf79176ab6453976
patriciaderieux <6089242+patty345@users.noreply.github.com>
2021-12-19 20:43:06 -0600

    fixed the code had bugs in them

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,38 +1,39 @@
 var questions = [
-    {
-        question: "When was the genre of 'neo country disco music' popularized?",
-        answers: [
-            {text: "80s", correct :true},
-            {text: "70s", correct: false},
-            {text: "60s", correct: false},
-            {text: "50s", correct: false}
-        ]
-    },
-    {
-        question: "Who wrote the iconic 'Rings Of Fire' in 1963?",
-        answers: [
-            {text: "Allen Jackson", correct: false},
-            {text: "Dolly Parton", correct: false},
-            {text: "Johnny Cash", correct: true},
-            {text: "Willie Nelson", correct: false}
-        ]
-    },
-    {
-        question: "Who wrote the record album 'Friends In Low Places'?",
-        answers: [
-            {text: "Toby Keith", correct: false},
-            {text: "Garth Brooks", correct: true},
-            {text: "Luke Bryan", correct: false},
-            {text: "Tim McGraw", correct: false}
-        ]
-    },
-    {
-        question: "Which Keith Urban album is his highest-selling, certified 4x Platinum?",
-        answers: [
-            {text: "Killin Time", correct: false},
-            {text: "Wouldn't it Be Great", correct: false},
-            {text: "Daddy Lesson", correct: false},
-            {text: "Be Here", correct: true}
-        ]
-    },
+  {
+    question: "When was the genre of 'neo country disco music' popularized?",
+    answers: [
+      { text: "80s", correct: true },
+      { text: "70s", correct: false },
+      { text: "60s", correct: false },
+      { text: "50s", correct: false },
+    ],
+  },
+  {
+    question: "Who wrote the iconic 'Rings Of Fire' in 1963?",
+    answers: [
+      { text: "Allen Jackson", correct: false },
+      { text: "Dolly Parton", correct: false },
+      { text: "Johnny Cash", correct: true },
+      { text: "Willie Nelson", correct: false },
+    ],
+  },
+  {
+    question: "Who wrote the record album 'Friends In Low Places'?",
+    answers: [
+      { text: "Toby Keith", correct: false },
+      { text: "Garth Brooks", correct: true },
+      { text: "Luke Bryan", correct: false },
+      { text: "Tim McGraw", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which Keith Urban album is his highest-selling, certified 4x Platinum?",
+    answers: [
+      { text: "Killin Time", correct: false },
+      { text: "Wouldn't it Be Great", correct: false },
+      { text: "Daddy Lesson", correct: false },
+      { text: "Be Here", correct: true },
+    ],
+  },
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,180 +1,216 @@
 var timeLeft = 75;
 var timerID;
 var timerEl = document.getElementById("timer");
-var startButton = document.getElementById("btn-start");
-var nextButton = document.getElementById("btn-next");
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
 var questionContainerEl = document.getElementById("question-container");
 var startContainerEl = document.getElementById("start-container");
 var questionEl = document.getElementById("question");
 var answerButtonsEl = document.getElementById("answer-buttons");
 var checkAnswerEl = document.getElementById("check-answer");
-var viewHighScores = document.getElementById("high-scores");
-var submitButton = document.getElementById("btn-submit");
-var clearScoreButton = document.getElementById("btn-clear");
+var viewHighScores = document.getElementById("highscores-link");
+var submitButton = document.getElementById("submit-btn");
+var clearScoreButton = document.getElementById("clear-btn");
 var initialsField = document.getElementById("player-name");
-var restartButton = document.getElementById("btn-restart");
+var restartButton = document.getElementById("restart-btn");
 var scoreField = document.getElementById("player-score");
 var scores = JSON.parse(localStorage.getItem("scores")) || [];
 
-var QuestionsSuffled, currentQuestionsIndex;
+var shuffledQuestions, currentQuestionIndex;
 
-startButton.addEventListener("click", startQuiz);
+
+// Start button trigger the first question and next button to display
+startButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
-  currentQuestionsIndex++;
-  setNextQuestion();
+    currentQuestionIndex++
+    setNextQuestion()
 });
 
-function clockTimer() {
-  timeLeft--;
-  timerEl.textContent = "Time: " + timeLeft;
-  if (timeLeft <= 0) {
-    saveScore();
-  }
+
+// Countdown timer
+function timeTick() {
+    timeLeft--;
+    timerEl.textContent = "Time: " + timeLeft;
+    if (timeLeft <= 0) {
+        saveScore();
+    }
 }
 
-function startQuiz() {
-  timerID = setInterval(clockTimer, 1000);
-  startContainerEl.classList.add("hide");
-  QuestionsSuffled = questions.sort(() => Math.random() - 0.5);
-  currentQuestionsIndex = 0;
-  questionContainerEl.classList.remove("hide");
 
-  clockTimer();
-  setNextQuestion;
-}
+// Start Quiz
+function startGame() {
+    timerID = setInterval(timeTick, 1000);
+    startContainerEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove("hide");
+
+    // Timer will start as soon as start button is clicked
+    timeTick();
+    setNextQuestion();
+};
+
+
 
 function setNextQuestion() {
-  resetState();
-  showQuestion(QuestionsSuffled[currentQuestionsIndex]);
-}
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
 
+
+// Display questions
 function showQuestion(question) {
-  questionEl.innerText = question.question;
-  question.answers.forEach((answer) => {
-    var button = document.createElement("button");
-    button.innerText = answer.text;
-    button.classList.add("btn");
-    if (answer.correct) {
-      button.dataset.correct = answer.correct;
-    }
-    button.addEventListener("click", selectAnswer);
-    answerButtonsEl.appendChild(button);
-  });
-}
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
+};
 
+
+// Reset state function
 function resetState() {
-  nextButton.classList.add("hide");
-  checkAnswerEl.classList.add("hide");
-  while (answerButtonsEl.firstChild) {
-    answerButtonsEl.removeChild(answerButtonsEl.firstChild);
-  }
-}
-
-function selectAnswer(e) {
-  var selectedButton = e.target;
-  var correct = selectedButton.dataset.correct;
-  checkAnswerEl.classList.remove("hide");
-  if (correct) {
-    checkAnswerEl.innerHTML = "Correct";
-  } else {
-    checkAnswerEl.innerHTML = "Wrong!";
-    if (timeLeft <= 10) {
-      timeLeft = 0;
-    } else {
-      timeLeft -= 10;
+    //clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    checkAnswerEl.classList.add("hide")
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild
+            (answerButtonsEl.firstChild)
     }
-  }
+};
 
-  Array.from(answerButtonsEl.children).forEach((button) => {
-    setStatusClass(button, button.dataset.correct);
-  });
-  if (QuestionsSuffled.length > currentQuestionsIndex + 1) {
-    nextButton.classList.remove("hide");
-    checkAnswerEl.classList.remove("hide");
-  } else {
-    startButton.classList.remove("hide");
-    saveScore();
-  }
-}
 
+// Select answer function
+function selectAnswer(e) {
+    var selectedButton = e.target;
+    //console.dir(selectedButton);
+    var correct = selectedButton.dataset.correct;
+    checkAnswerEl.classList.remove("hide")
+    // Check if the answer correct or wrong then show text
+    if (correct) {
+        checkAnswerEl.innerHTML = "You got it right!";
+    } else {
+        checkAnswerEl.innerHTML = "Sorry that was not the correct answer.";
+        if (timeLeft <= 10) {
+            timeLeft = 0;
+        } else {
+            // If the aswer is wrong, deduct time by 10
+            timeLeft -= 10;
+        }
+    }
+
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+        checkAnswerEl.classList.remove("hide")
+    } else {
+        startButton.classList.remove("hide")
+        saveScore();
+    }
+};
+
+
+// Check and show the correct answer by set the buttons colors
 function setStatusClass(element, correct) {
-  clearStatusClass(element);
-  if (correct) {
-    element.classList.add("correct");
-  } else {
-    element.classList.add("wrong");
-  }
-}
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+};
 
+
+// Remove all the classes
 function clearStatusClass(element) {
-  element.classList.remove("correct");
-  element.classList.remove("wrong");
-}
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+};
 
+
+// Save scores
 function saveScore() {
-  clearInterval(timerID);
-  timerEl.textContent = "Time: " + timeLeft;
-  setTimeout(function () {
-    questionContainerEl.classList.add("hide");
-    document.getElementById("score-container").classList.remove("hide");
-    document.getElementById("your-score").textContent =
-      "Your final score is " + timeLeft;
-  }, 2000);
-}
+    clearInterval(timerID);
+    timerEl.textContent = "Time: " + timeLeft;
+    setTimeout(function () {
+        
+        questionContainerEl.classList.add("hide");
+        document.getElementById("score-container").classList.remove("hide");
+        document.getElementById("your-score").textContent = "Your final score is " + timeLeft;
+
+    }, 2000)
+};
+
 
 var loadScores = function () {
-  if (!savedScores) {
-    return false;
-  }
+    // Get score from local storage
 
-  savedScores = JSON.parse(savedScores);
-  var initials = document.querySelector("#initials-field").value;
-  var newScore = {
-    score: timeLeft,
-    initials: initials,
-  };
+    if (!savedScores) {
+        return false;
+    }
 
-  savedScores.push(newScore);
-  console.log(savedScores);
+    // Convert scores from stringfield format into array
+    savedScores = JSON.parse(savedScores);
+    var initials = document.querySelector("#initials-field").value;
+    var newScore = {
+        score: timeLeft,
+        initials: initials
+    }
+    savedScores.push(newScore);
+    console.log(savedScores)
 
-  savedScores.forEach((score) => {
-    initialsField.innerText = score.initials;
-    scoreField.innerText = score.score;
-  });
+    savedScores.forEach(score => {
+        initialsField.innerText = score.initials
+        scoreField.innerText = score.score
+    })
 };
+
+
 
 function showHighScores(initials) {
-  document.getElementById("highscores").classList.remove("hide");
-  document.getElementById("score-container").classList.add("hide");
-  startContainerEl.classList.add("hide");
-  questionContainerEl.classList.add("hide");
-  if (typeof initials == "string") {
-    var score = {
-      initials,
-      timeLeft,
-    };
-    scores.push(score);
-  }
+    document.getElementById("highscores").classList.remove("hide")
+    document.getElementById("score-container").classList.add("hide");
+    startContainerEl.classList.add("hide");
+    questionContainerEl.classList.add("hide");
+    if (typeof initials == "string") {
+        var score = {
+            initials, timeLeft
+        }
+        scores.push(score)
+    }
 
-  var highScoreEl = document.getElementById("highscore");
-  highScoreEl.innerHTML = "";
+    var highScoreEl = document.getElementById("highscore");
+    highScoreEl.innerHTML = "";
+    
+    for (i = 0; i < scores.length; i++) {
+        var div1 = document.createElement("div");
+        div1.setAttribute("class", "name-div");
+        div1.innerText = scores[i].initials;
+        var div2 = document.createElement("div");
+        div2.setAttribute("class", "score-div");
+        div2.innerText = scores[i].timeLeft;
 
-  for (i = 0; i < scores.length; i++) {
-    var div1 = document.createElement("div");
-    div1.setAttribute("class", "name-div");
-    div1.innerText = scores[i].initials;
-    var div2 = document.createElement("div");
-    div2.setAttribute("class", "score-div");
-    div2.innerText = scores[i].timeLeft;
+        highScoreEl.appendChild(div1);
+        highScoreEl.appendChild(div2);
+    }
 
-    highScoreEl.appendChild(div1);
-    highScoreEl.appendChild(div2);
-  }
+    localStorage.setItem("scores", JSON.stringify(scores));
 
-  localStorage.setItem("scores", JSON.stringify(scores));
 };
 
+
+
 viewHighScores.addEventListener("click", showHighScores);
+
 
 submitButton.addEventListener("click", function (event) {
     event.preventDefault()
@@ -183,9 +219,12 @@ submitButton.addEventListener("click", function (event) {
 });
 
 
+
 restartButton.addEventListener("click", function () {
     window.location.reload();
 });
+
+
 
 clearScoreButton.addEventListener("click", function () {
     localStorage.clear();
