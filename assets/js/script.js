94e45e4efac35c2ea613529fa8e9bbd8faafd4ed
patriciaderieux <6089242+patty345@users.noreply.github.com>
2021-12-19 18:26:36 -0600

    adding styles to header, h1, h2

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var questionEl = document.getElementById("question");
 var answerButtonsEl = document.getElementById("answer-buttons");
 var checkAnswerEl = document.getElementById("check-answer");
 var viewHighScores = document.getElementById("high-scores");
-var submitButton =  document.getElementById("btn-submit");
+var submitButton = document.getElementById("btn-submit");
 var clearScoreButton = document.getElementById("btn-clear");
 var initialsField = document.getElementById("player-name");
 var restartButton = document.getElementById("btn-restart");
@@ -20,54 +20,79 @@ var QuestionsSuffled, currentQuestionsIndex;
 
 startButton.addEventListener("click", startQuiz);
 nextButton.addEventListener("click", () => {
-    currentQuestionsIndex++
-    setNextQuestion()
+  currentQuestionsIndex++;
+  setNextQuestion();
 });
 
 function clockTimer() {
-    timeLeft--;
-    timerEl.textContent = "Time: " + timeLeft;
-    if (timeLeft <= 0) {
-        saveScore();  
-      }
+  timeLeft--;
+  timerEl.textContent = "Time: " + timeLeft;
+  if (timeLeft <= 0) {
+    saveScore();
+  }
 }
 
 function startQuiz() {
-    timerID = setInterval(clockTimer, 1000);
-    startContainerEl.classList.add("hide");
-    QuestionsSuffled = questions.sort(() => Math.random() - .5)
-    currentQuestionsIndex = 0
-    questionContainerEl.classList.remove("hide");
+  timerID = setInterval(clockTimer, 1000);
+  startContainerEl.classList.add("hide");
+  QuestionsSuffled = questions.sort(() => Math.random() - 0.5);
+  currentQuestionsIndex = 0;
+  questionContainerEl.classList.remove("hide");
 
-    clockTimer();
-    setNextQuestion;
-};
+  clockTimer();
+  setNextQuestion;
+}
 
-function setNextQuestion () {
-    resetState();
-    showQuestion(QuestionsSuffled[currentQuestionsIndex]);
-};
+function setNextQuestion() {
+  resetState();
+  showQuestion(QuestionsSuffled[currentQuestionsIndex]);
+}
 
 function showQuestion(question) {
-    questionEl.innerText = question.question
-    question.answers.forEach(answer => {
-        var button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsEl.appendChild(button)
-    })
-};
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsEl.appendChild(button);
+  });
+}
 
 function resetState() {
-    nextButton.classList.add("hide")
-    checkAnswerEl.classList.add("hide")
-    while(answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+  nextButton.classList.add("hide");
+  checkAnswerEl.classList.add("hide");
+  while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  var selectedButton = e.target;
+  var correct = selectedButton.dataset.correct;
+  checkAnswerEl.classList.remove("hide");
+  if (correct) {
+    checkAnswerEl.innerHTML = "Correct";
+  } else {
+    checkAnswerEl.innerHTML = "Wrong!";
+    if (timeLeft <= 10) {
+      timeLeft = 0;
+    } else {
+      timeLeft -= 10;
     }
-};
+  }
 
-
+  Array.from(answerButtonsEl.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (QuestionsSuffled.length > currentQuestionsIndex + 1) {
+    nextButton.classList.remove("hide");
+    checkAnswerEl.classList.remove("hide");
+  } else {
+    startButton.classList.remove("hide");
+    saveScore();
+  }
+}
